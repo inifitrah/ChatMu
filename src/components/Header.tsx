9dbeee4f5cc6b96e1bfcc62b0c378eeb0100417d
fitrah <mftrhr@gmail.com>
2024-10-09 20:41:57 +0800

    fix: wrong import dropdown menu in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import Menu from "./Menu";
+
+import { EllipsisVertical } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-import { EllipsisVertical } from "lucide-react";
+} from "./ui/dropdown-menu";
 
 const Header = () => {
   return (
@@ -20,11 +20,8 @@ const Header = () => {
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-52 fixed top-2 -right-4 flex flex-col gap-1 bg-black text-white  rounded-md p-5">
           <DropdownMenuItem className="px-1 ">Profile</DropdownMenuItem>
-          <DropdownMenuSeparator />
           <DropdownMenuItem className="px-1">Settings</DropdownMenuItem>
-          <DropdownMenuSeparator />
           <DropdownMenuItem className="px-1">Help</DropdownMenuItem>
-          <DropdownMenuSeparator />
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
