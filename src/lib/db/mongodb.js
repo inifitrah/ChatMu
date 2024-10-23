b7d60b96bc54a50b312a48842839d706ab8c6064
fitrah <mftrhr@gmail.com>
2024-10-23 19:44:27 +0800

    database connection configuration: error handle when there is no environment variable for MONGO_URI, add url path for database name and console log when mongodb is connected, error and disconnected

--- src/lib/db/mongodb.js
+++ src/lib/db/mongodb.js
@@ -1,10 +1,33 @@
 import mongoose from "mongoose";
 
+const { MONGO_USER, MONGO_PASSWORD, MONGO_DB } = process.env;
+
+if (!MONGO_USER || !MONGO_PASSWORD || !MONGO_DB) {
+  console.error(
+    "Error: Missing required environment variables MONGO_USER, MONGO_PASSWORD, or MONGO_DB"
+  );
+  process.exit(1);
+}
+
+const MONGO_URI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:27017/${MONGO_DB}?authSource=admin`;
+
 export const connectToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://root:admin123@localhost:27017");
+    await mongoose.connect(MONGO_URI);
   } catch (error) {
     console.error(`Error: ${error}`);
     process.exit(1);
   }
 };
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log("Error connecting to MongoDB", error);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from MongoDB");
+});
