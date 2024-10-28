ddc5182df7f2e5a630e55bbce1954901ac5ca29c
fitrah <mftrhr@gmail.com>
2024-10-28 16:02:56 +0800

    add: next auth for signin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SessionWrapper from "@/components/SessionWrapper";
 
 export const metadata: Metadata = {
   title: "ChatMu",
@@ -12,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="text-black">{children}</body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body className="text-black">{children}</body>
+      </html>
+    </SessionWrapper>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "@/components/Header";
 import ChatList from "@/components/ChatList";
+import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (!session) {
+      redirect("/signin");
+    }
+  }, [session]);
   return (
     <>
       <Header />
