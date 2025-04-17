71889a57b0fc08681e6c743214710721b5eef2a2
fitrah <mftrhr@gmail.com>
2025-04-17 19:37:34 +0800

    feat: update header component to display user avatar from session

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,14 +6,16 @@ import NavigationMenu from "@/components/header/NavigationMenu";
 import { Input } from "../ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { ThemeSwitch } from "../ThemeSwitch";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
   return (
     <header className="text-black flex flex-col px-8 pt-8 pb-2 bg-[#F8F8F8] dark:bg-[#0D0F12] dark:text-white">
       <div className="flex items-center justify-between mb-6 ">
         <div className="flex items-center gap-4">
           <Avatar className="w-12 h-12">
-            <AvatarImage src={"session.user.image"} />
+            <AvatarImage src={session?.user.image} />
             <AvatarFallback>
               <CircleUser size={60} />
             </AvatarFallback>
