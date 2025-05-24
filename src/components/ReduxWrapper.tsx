05115414840c3080306d30efaad7a02052fe441e
fitrah <mftrhr@gmail.com>
2025-05-25 07:29:42 +0800

    feat(ReduxWrapper): wrap children with ConversationProviderWrapper

--- src/components/ReduxWrapper.tsx
+++ src/components/ReduxWrapper.tsx
@@ -3,9 +3,14 @@
 import { store } from "@/redux-toolkit/store";
 import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
+import ConversationProviderWrapper from "./ConversationProviderWrapper";
 
 const ReduxWrapper = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <ConversationProviderWrapper>{children}</ConversationProviderWrapper>
+    </Provider>
+  );
 };
 
 export default ReduxWrapper;
