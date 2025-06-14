8bbb93460035be571e51c5e0ac05e343e8e02270
fitrah <mftrhr@gmail.com>
2025-06-14 17:12:17 +0800

    refactor(socket): enhance context type and streamline message handling

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -14,12 +14,15 @@ import { IMessage } from "@/types/conversation";
 type ContextType = {
   socket: Socket | undefined;
   connected: boolean;
+
   sendMessage: (data: IMessage) => void;
   markAsDelivered: (data: { senderId: string; conversationId: string }) => void;
-  listenMessage: (callback: (data: IMessage) => void) => void;
+  markAsRead: (data: { conversationId: string; userId: string }) => void;
+
+  listenMessage: (callback: (data: IMessage) => void) => { off: () => void };
   listenMessageReceived: (
     callback: (data: { senderId: string; conversationId: string }) => void
-  ) => void;
+  ) => { off: () => void };
   listenMessageSent: (
     callback: (data: {
       tempId: string;
@@ -27,30 +30,29 @@ type ContextType = {
       timeStamp: number;
       conversationId: string;
     }) => void
-  ) => void;
-  markAsRead: (data: { conversationId: string; userId: string }) => void;
-  listenMarkAsRead: (callback: (data: any) => void) => void;
+  ) => { off: () => void };
+  listenMessageRead: (callback: (data: any) => void) => { off: () => void };
   listenOnlineUsers: (
     callback: (
       data: { userId: string; username: string; socketId: string }[]
     ) => void
-  ) => void;
+  ) => { off: () => void };
 };
 
 const defaultValue: ContextType = {
   socket: undefined,
-  sendMessage() {},
-  markAsDelivered() {},
-  listenMessageSent() {},
-  listenMessageReceived() {},
-  listenMessage() {},
-  markAsRead() {},
-  listenMarkAsRead() {},
-  listenOnlineUsers() {},
+  sendMessage: () => {},
+  markAsDelivered: () => {},
+  listenMessageSent: () => ({ off: () => {} }),
+  listenMessageReceived: () => ({ off: () => {} }),
+  listenMessage: () => ({ off: () => {} }),
+  markAsRead: () => {},
+  listenMessageRead: () => ({ off: () => {} }),
+  listenOnlineUsers: () => ({ off: () => {} }),
   connected: false,
 };
 
-const SocketContext = createContext(defaultValue);
+const SocketContext = createContext<ContextType>(defaultValue);
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
   const [socket, setSocket] = useState<Socket | undefined>(undefined);
@@ -80,9 +82,6 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
       });
       setSocket(socket);
       return () => {
-        socket.off("connect");
-        socket.off("getOnlineUsers");
-        socket.off("disconnect");
         if (socket && socket.connected) {
           socket.disconnect();
           setAlreadyConnect(false);
@@ -95,63 +94,52 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
       }
       return;
     }
-  }, [session]);
+  }, [session, toast]);
 
-  const value: ContextType = useMemo(
-    () => ({
+  const value: ContextType = useMemo(() => {
+    const createListener =
+      (eventName: string) => (callback: (data: any) => void) => {
+        if (socket) {
+          socket.on(eventName, callback);
+          return {
+            off: () => {
+              socket.off(eventName, callback);
+            },
+          };
+        } else {
+          console.warn(
+            `Socket is not initialized. Cannot listen to event: ${eventName}`
+          );
+          return {
+            off: () => {},
+          };
+        }
+      };
+
+    const createEmitter = (eventName: string) => (data: any) => {
+      if (socket) {
+        socket.emit(eventName, data);
+      } else {
+        console.warn(
+          `Socket is not initialized. Cannot emit event: ${eventName}`
+        );
+      }
+    };
+
+    return {
       socket,
       connected: alreadyConnect,
-      sendMessage(data) {
-        if (socket) {
-          socket.off("client:send_message");
-          socket.emit("client:send_message", data);
-        }
-      },
-      listenMessageReceived(callback) {
-        if (socket) {
-          socket.off("server:message_received");
-          socket.on("server:message_received", callback);
-        }
-      },
-      markAsDelivered(data) {
-        if (socket) {
-          socket.off("client:message_received");
-          socket.emit("client:message_received", data);
-        }
-      },
-      listenMessageSent(callback) {
-        if (socket) {
-          socket.off("server:message_sent");
-          socket.on("server:message_sent", callback);
-        }
-      },
-      listenMessage(callback) {
-        if (socket) {
-          socket.off("server:new_message");
-          socket.on("server:new_message", callback);
-        }
-      },
-      markAsRead(data) {
-        if (socket) {
-          socket.off("mark_as_read");
-          socket.emit("mark_as_read", data);
-        }
-      },
-      listenMarkAsRead(callback) {
-        if (socket) {
-          socket.off("mark_as_read");
-          socket.on("mark_as_read", callback);
-        }
-      },
-      listenOnlineUsers(callback) {
-        if (socket) {
-          socket.off("get_online_users");
-          socket.on("get_online_users", callback);
-        }
-      },
-    }),
-    [alreadyConnect, socket]
-  );
+      sendMessage: createEmitter("client:send_message"),
+      listenMessageReceived: createListener("server:message_received"),
+      markAsDelivered: createEmitter("client:message_received"),
+      listenMessageSent: createListener("server:message_sent"),
+      listenMessage: createListener("server:new_message"),
+      markAsRead: createEmitter("client:message_read"),
+      listenMessageRead: createListener("server:mark_as_read"),
+      listenOnlineUsers: createListener("get_online_users"),
+    };
+  }, [alreadyConnect, socket]);
+
   return (
     <SocketContext.Provider value={value}>{children}</SocketContext.Provider>
   );
