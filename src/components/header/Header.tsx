aa238499593451c83d3c9962ac4a81f91d15c3eb
fitrah <mftrhr@gmail.com>
2025-06-15 06:30:59 +0800

    refactor(Header): replace user avatar with logo images and adjust layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,29 @@
 "use client";
 import React from "react";
-
-import { CircleUser } from "lucide-react";
+import Image from "next/image";
 import NavigationMenu from "@/components/header/NavigationMenu";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { ThemeSwitch } from "../ThemeSwitch";
-import { useSession } from "next-auth/react";
 import ConversationSearch from "@/components/conversation/ConversationSearch";
 
 const Header = () => {
-  const { data: session } = useSession();
   return (
-    <header className="text-black flex flex-col px-8 pt-8 pb-2 bg-[#F8F8F8] dark:bg-[#0D0F12] dark:text-white">
+    <header className="text-black flex flex-col px-7 pt-8 pb-2 bg-[#F8F8F8] dark:bg-[#0D0F12] dark:text-white">
       <div className="flex items-center justify-between mb-6 ">
-        <div className="flex items-center gap-4">
-          <Avatar className="w-12 h-12">
-            <AvatarImage src={session?.user.image} />
-            <AvatarFallback>
-              <CircleUser size={60} />
-            </AvatarFallback>
-          </Avatar>
-          <h1 className="text-3xl font-bold text-black dark:text-white">
-            ChatMu
-          </h1>
+        <div>
+          <Image
+            width={65}
+            height={65}
+            className="rounded-full block dark:hidden"
+            alt="Logo"
+            src={"/svg/logo.svg"}
+          />
+          <Image
+            width={65}
+            height={65}
+            className="rounded-full dark:block hidden"
+            alt="Logo"
+            src={"/svg/darklogo.svg"}
+          />
         </div>
         <div className="flex items-center gap-4">
           <ThemeSwitch />
