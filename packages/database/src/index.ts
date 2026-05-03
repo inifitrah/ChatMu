76052224c4ae16b9716cbeebc7a4664bdd5737fb
fitrah <mftrhr@gmail.com>
2026-05-04 07:05:46 +0800

    refactor(db): use explicit index file paths in exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,10 +2,10 @@
 export * from "./mongodb";
 
 // Export models
-export * from "./models";
+export * from "./models/index";
 
 // Export data access
-export * from "./data-access";
+export * from "./data-access/index";
 
 // Export utils
-export * from "./utils";
+export * from "./utils/index";
